f2b592b92dec163817ece63e1230ba509e4399b1
jxnkwlp <jxnkwlp@hotmail.com>
2023-09-23 17:00:15 +0800

    remove invalid project

--- framework/src/Passingwind.Authentication.Saml2/Saml2Defaults.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Passingwind.Authentication.Saml2;
-
-public static class Saml2Defaults
-{
-    public const string AuthenticationScheme = "Saml2";
-
-    public const string DisplayName = "Saml2";
-}
