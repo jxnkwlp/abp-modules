839ca41000d85d51ff4509b2209f94b91e01f604
jxnkwlp <jxnkwlp@hotmail.com>
2023-09-26 15:54:37 +0800

    fix: file management permission incorrect parent

--- modules/file-management/src/Passingwind.Abp.FileManagement.Application.Contracts/Permissions/FileManagementPermissionDefinitionProvider.cs
+++ modules/file-management/src/Passingwind.Abp.FileManagement.Application.Contracts/Permissions/FileManagementPermissionDefinitionProvider.cs
@@ -6,14 +6,14 @@ namespace Passingwind.Abp.FileManagement.Permissions;
 
 public class FileManagementPermissionDefinitionProvider : PermissionDefinitionProvider
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Roslynator", "RCS1201:Use method chaining.", Justification = "<Pending>")]
     public override void Define(IPermissionDefinitionContext context)
     {
         var fileManagement = context.AddGroup(FileManagementPermissions.GroupName, L($"Permission:{FileManagementPermissions.GroupName}"));
 
         var fileContainer = fileManagement.AddPermission(FileManagementPermissions.FileContainer.Default, L($"Permission:{FileManagementPermissions.FileContainer.Default}"));
-        fileContainer
-            .AddChild(FileManagementPermissions.FileContainer.Update, L($"Permission:{FileManagementPermissions.FileContainer.Update}"))
-            .AddChild(FileManagementPermissions.FileContainer.Delete, L($"Permission:{FileManagementPermissions.FileContainer.Delete}"));
+        fileContainer.AddChild(FileManagementPermissions.FileContainer.Update, L($"Permission:{FileManagementPermissions.FileContainer.Update}"));
+        fileContainer.AddChild(FileManagementPermissions.FileContainer.Delete, L($"Permission:{FileManagementPermissions.FileContainer.Delete}"));
 
         var file = fileManagement.AddPermission(FileManagementPermissions.File.Default, L($"Permission:{FileManagementPermissions.File.Default}"));
         file.AddChild(FileManagementPermissions.File.Delete, L($"Permission:{FileManagementPermissions.File.Delete}"));
