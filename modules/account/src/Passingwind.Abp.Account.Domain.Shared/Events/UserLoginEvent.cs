fc152693a5834bb4d73e9f4a967e4136bca97220
jxnkwlp <jxnkwlp@hotmail.com>
2024-05-16 22:15:24 +0800

    feat: add account login event

---
+++ modules/account/src/Passingwind.Abp.Account.Domain.Shared/Events/UserLoginEvent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Passingwind.Abp.Account.Events;
+
+[Serializable]
+public class UserLoginEvent
+{
+    public const string PasswordLogin = "PasswordLogin";
+    public const string AuthenticatorRecoveryCodeLogin = "AuthenticatorRecoveryCodeLogin";
+    public const string TwoFactorLogin = "TwoFactorLogin";
+    public const string Logout = "Logout";
+    public const string ExternalLogin = "ExternalLogin";
+    public const string ImpersonationLogin = "ImpersonationLogin";
+    public const string ImpersonationLogout = "ImpersonationLogout";
+    public const string LinkLogin = "LinkLogin";
+    public const string DelegationLogin = "DelegationLogin";
+
+    public UserLoginEvent(Guid userId, string @event, Dictionary<string, object>? eventData = null)
+    {
+        UserId = userId;
+        Event = @event;
+        EventData = eventData ?? new Dictionary<string, object>();
+    }
+
+    public Guid UserId { get; set; }
+
+    public string Event { get; set; } = null!;
+
+    public Dictionary<string, Object> EventData { get; }
+}
