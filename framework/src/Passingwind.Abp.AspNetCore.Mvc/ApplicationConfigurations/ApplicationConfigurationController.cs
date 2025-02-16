422f55e2f7aeda8103df9e870a0ea2e8ba17c0ae
jxnkwlp <jxnkwlp@hotmail.com>
2025-02-16 14:19:55 +0800

    feat: mvc framework module

---
+++ framework/src/Passingwind.Abp.AspNetCore.Mvc/ApplicationConfigurations/ApplicationConfigurationController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.AntiForgery;
+using Volo.Abp.AspNetCore.Mvc.ApplicationConfigurations;
+
+namespace Passingwind.Abp.AspNetCore.Mvc.ApplicationConfigurations;
+
+[Area("Application")]
+[Route("api/application/configuration")]
+public class ApplicationConfigurationController : IApplicationConfigurationAppService
+{
+    protected IApplicationConfigurationAppService Service { get; }
+    protected readonly IAbpAntiForgeryManager AntiForgeryManager;
+
+    public ApplicationConfigurationController(IApplicationConfigurationAppService service, IAbpAntiForgeryManager antiForgeryManager)
+    {
+        Service = service;
+        AntiForgeryManager = antiForgeryManager;
+    }
+
+    [HttpGet]
+    public virtual Task<ApplicationConfigurationDto> GetAsync(ApplicationConfigurationRequestOptions options)
+    {
+        AntiForgeryManager.SetCookie();
+
+        return Service.GetAsync(options);
+    }
+}
---
+++ framework/src/Passingwind.Abp.AspNetCore.Mvc/AspNetCoreMvcModule.cs
@@ -0,0 +1,13 @@
+﻿using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.Modularity;
+
+namespace Passingwind.Abp.AspNetCore.Mvc;
+
+[DependsOn(typeof(AbpAspNetCoreMvcModule))]
+[DependsOn(typeof(AspNetCoreMvcModuleContracts))]
+public class AspNetCoreMvcModule : AbpModule
+{
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+    }
+}
